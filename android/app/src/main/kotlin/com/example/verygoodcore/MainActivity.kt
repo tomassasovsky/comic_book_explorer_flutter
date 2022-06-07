963edd1f5ae9f5dbfbef542ff83cbe5c001fa790
tomassasovsky <63405252+tomassasovsky@users.noreply.github.com>
2022-06-07 11:12:17 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.comic_book_explorer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:comic_book_explorer/app/app.dart';
+import 'package:comic_book_explorer/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
